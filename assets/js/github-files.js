b15de4c50d5522e0cd66c572a94d6a0d53d8face
Giau Ngo <giaunv@users.noreply.github.com>
2019-02-20 13:14:52 +0000

    Update Wed Feb 20 13:14:30 UTC 2019

--- assets/js/github-files.js
+++ assets/js/github-files.js
@@ -4,7 +4,7 @@
       code = $(code);
 
       var filePath = /github.com\/(\S*)/i.exec(code.attr('class'))[1];
-      var fileName = filePath.substring((filePath.indexOf('/src') > -1 ? filePath.indexOf('/src') : filePath.lastIndexOf('/'))+1);
+      var fileName = filePath.substring(filePath.lastIndexOf('/')+1);
       var linkToGithubFile = $("<p><a target='_blank' href='" + "https://github.com/" + filePath + "'>" + fileName + "</a></p>");
 
       var fullFilePath = /embedfile-(\S*)/i.exec(code.attr('class'))[1];
