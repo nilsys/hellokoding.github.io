914fcf52aa85fbf54d88338c5655b8a5fde9c872
Giau Ngo <giaunv@users.noreply.github.com>
2019-05-10 15:32:18 +0000

    Update Fri May 10 15:32:00 UTC 2019

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -18,14 +18,29 @@
             var options = {
                 offset: {
                     top: 10,
-                    bottom: $('footer').height() + 64
+                    bottom: $('footer').height() + 64*2
                 }
             }
             $.stickysidebarscroll(stickyItem, options);
         }
     };
 
+    var unSticky = function() {
+        var stickyItem = findStickyItem();
+        if (stickyItem) {
+            $(window).unbind('scroll resize orientationchange load', stickyItem);
+        }
+    }
+
     $(function () {
-        sticky();
+        $(window).resize(function() {
+            var w = $(window).width() / parseFloat($("body").css("font-size"));
+            if (w >= 64.063) {
+                sticky();
+            }
+            else {
+                unSticky();
+            }
+        });
     });
 })(jQuery);
